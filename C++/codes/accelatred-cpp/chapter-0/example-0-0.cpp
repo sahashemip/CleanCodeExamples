d7c70ae8ac758f9dc1f3aea6e0978bb8d4083e17
arsalan <arsi@yoga.arsi>
2024-10-11 12:33:57 +0300

    Reorganize repo and add exercises

---
+++ C++/codes/accelatred-cpp/chapter-0/example-0-0.cpp
@@ -0,0 +1,43 @@
+// #include <iostream>
+
+// int main(int argc, char* argv[]) {
+//     using std::cout;
+//     cout << "Hello, World!\n";
+
+//     return 0;
+// }
+
+
+// #include <iostream>
+
+// int main(int argc, char* argv[]) {
+//     std::cout << "Number of argunments: " << argc << "\n";
+
+//     for (int i = 0; i < argc; ++i) {
+//         std::cout << "Argument " << i << ":" << argv[i] << "\n";
+//     }
+
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
